500476fe71713cbb053e08bbaf7dd8dff5ac4c27
hungnguyenthanh <nguyen.thanh.hung@framgia.com>
2016-05-18 14:04:08 +0700

    Add feature filter image

---
+++ app/src/main/java/com/framgia/photoalbum/effect/Pixelate.java
@@ -0,0 +1,39 @@
+package com.framgia.photoalbum.effect;
+
+import android.graphics.Bitmap;
+import android.support.v8.renderscript.Allocation;
+import android.support.v8.renderscript.RenderScript;
+
+import com.framgia.photoalbum.ScriptC_PixelateFilter;
+import com.framgia.photoalbum.ui.activity.EditActivity;
+
+/**
+ * Created by HungNT on 5/12/16.
+ */
+public class Pixelate extends EffectFilter {
+
+    @Override
+    public Bitmap applyEffect(Bitmap src) {
+        Bitmap bmOut = Bitmap.createBitmap(src.getWidth(), src.getHeight(), src.getConfig());
+
+        RenderScript rs = RenderScript.create(EditActivity.sContext);
+
+        Allocation allocationIn = Allocation.createFromBitmap(rs, src);
+        Allocation allocationOut = Allocation.createTyped(rs, allocationIn.getType());
+
+        ScriptC_PixelateFilter filter = new ScriptC_PixelateFilter(rs);
+        filter.set_gIn(allocationIn);
+        filter.set_gOut(allocationOut);
+        filter.set_gSquareSize(15);
+        filter.invoke_filter();
+
+        allocationOut.copyTo(bmOut);
+
+        rs.destroy();
+        allocationIn.destroy();
+        allocationOut.destroy();
+        filter.destroy();
+
+        return bmOut;
+    }
+}
