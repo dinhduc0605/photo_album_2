e1a2ca17b5c7cb4af8f5dc060a3d320d6c21dc54
Nguyen Dinh Duc <dinhduc0605@gmail.com>
2016-06-15 14:33:46 +0700

    create transition effects

--- app/src/main/java/com/framgia/photoalbum/util/DimenUtils.java
+++ app/src/main/java/com/framgia/photoalbum/util/DimenUtils.java
@@ -1,6 +1,8 @@
 package com.framgia.photoalbum.util;
 
 import android.content.Context;
+import android.util.DisplayMetrics;
+import android.view.WindowManager;
 
 /**
  * Created by HungNT on 5/2/16.
@@ -9,6 +11,14 @@ public class DimenUtils {
 
     public static float dpToPx(Context context, float dp) {
         return dp * context.getResources().getDisplayMetrics().density;
+    }
+    public static DisplayMetrics getDisplayMetrics(Context context){
+        DisplayMetrics metrics = new DisplayMetrics();
+        ((WindowManager) context
+                .getSystemService(Context.WINDOW_SERVICE))
+                .getDefaultDisplay()
+                .getMetrics(metrics);
+        return metrics;
     }
 
 }
